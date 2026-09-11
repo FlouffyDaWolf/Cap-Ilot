02e80089f3c2c552600697bc50aaa4cb7d5f04f6
FlouffyDaWolf <evanmideo@hotmail.com>
2026-09-11 17:10:52 +0200

    cvjhe dgvjn,df bkjn

--- Assets/Game/3-Scripts/Diary/Diary.cs
+++ Assets/Game/3-Scripts/Diary/Diary.cs
@@ -2,8 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Globalization;
 using TMPro;
-using UnityEditor.Rendering;
-using UnityEditor.SceneManagement;
 using UnityEngine;
 
 [Serializable]
